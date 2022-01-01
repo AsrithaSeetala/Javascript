3909a0879371487fff64236c6b8d686e7338f37c
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-02 00:07:34 +0530

    It is a js file

---
+++ Loops/loops.js
@@ -0,0 +1,17 @@
+/* var i = 1;
+
+while (i < 5) {
+
+document.write ("It should run 4 times ");
+
+i++; 
+
+} 
+
+*/
+
+for (var i = 1; i < 5; i++) {
+
+document.write ("It should run 4 times ");
+
+}
