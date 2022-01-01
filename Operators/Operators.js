ffeeba6cb67347effd91af6f6bc16d1c474ad9de
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-01 21:59:38 +0530

    It is a js file

---
+++ Operators/Operators.js
@@ -0,0 +1,20 @@
+// Arithmetic and Assignment Operators
+// Increment and Decrement Operators
+
+var number1 = 10;
+var number2 = 20;
+
+// var number1 = number1 + number2;
+
+number1--;
+number1 += number2;
+number1++;
+number1 -= number2;
+
+var number2 = number1 * number2;
+var number2 = number2/number1;
+
+// The division and multiplication operators take precedence over the addition and subtraction operators
+
+var number1 = (2 + 3 * 4 / 5) - 1;
+document.write (number1);
