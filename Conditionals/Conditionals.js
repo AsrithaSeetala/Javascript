5e769f3b736d2053bc7b193d3746f080cc6d6d41
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-01 22:03:42 +0530

    It is a js file

---
+++ Conditionals/Conditionals.js
@@ -0,0 +1,21 @@
+var item = "lipstick";
+
+if (item == "Kajal")
+{
+    alert ("I would occasionally wear Kajal");
+}
+
+else if (item == "Bangles") 
+{
+    alert ("I would occasionally wear Bangles");
+}
+
+else if (item == "lipstick") 
+{
+    alert ("I would occasionally wear lipstick");
+}
+
+else 
+{
+    alert ("I would occasionally wear Kajal");
+}
