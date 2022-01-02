c0dac9a2bab8df632855268660e1a112e3d5d533
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-02 21:25:00 +0530

    a js file of getElementsByClassName

---
+++ getElementsByClassName/getElementsByClassName.js
@@ -0,0 +1,13 @@
+function changeStyle() {
+    
+    var paragraph = document.getElementsByClassName ("para");
+
+    var changeText = paragraph[0].style.color = "Green";
+
+    var changeText = paragraph[1].style.color = "purple";
+
+    var changeText = paragraph[2].style.color = "red";
+
+    var changeText = paragraph[3].style.color = "blue";
+
+}
