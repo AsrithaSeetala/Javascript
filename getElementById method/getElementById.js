a478095e6d6f4e20736642d262a5b4462bcf7146
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-02 20:30:57 +0530

    a js file of getElementById method

---
+++ getElementById method/getElementById.js
@@ -0,0 +1,9 @@
+function changeStyle() {
+
+  var text = document.getElementById ("p1").style.color = "yellow";
+
+  var text = document.getElementById ("p1").style.backgroundColor = "blue";
+
+  var text = document.getElementById ("p1").style.fontStyle = "italic";
+
+}
