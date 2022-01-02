6fb2f03ecae8b7f4d209ebc9a6c27348eecaa071
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-02 21:47:55 +0530

    a js file of Reading content using innerHTML

---
+++ Reading content using innerHTML/Reading content using innerHTML.js
@@ -0,0 +1,15 @@
+function changeStyle() {
+    
+    var paragraph = document.getElementsByClassName ("para");
+
+    var element1 = paragraph[0].innerHTML;
+    var element2 = paragraph[1].innerHTML;
+    var element3 = paragraph[2].innerHTML;
+    var addThem = paragraph[3].innerHTML = element1 + element2 + element3;
+
+
+    var element1 = paragraph[0].innerHTML="";
+    var element2 = paragraph[1].innerHTML="";
+    var element3 = paragraph[2].innerHTML="";
+
+}
