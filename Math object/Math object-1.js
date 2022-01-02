d07871b6a9504439d4b4adc486fc7ab7e87ed60d
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-02 19:57:52 +0530

    a js file of Math object 1

---
+++ Math object/Math object-1.js
@@ -0,0 +1,11 @@
+var number = 5.7;
+ 
+// var newNumber = 30 + Math.ceil(number);
+
+var newNumber = 30 + Math.round(number);
+
+// var newNumber = 30 + Math.floor(number);
+
+document.write(newNumber);
+
+// Math object in Javascript
