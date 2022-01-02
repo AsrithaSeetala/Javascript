0f7d93bcafbff6f768c95a27244cde43f4e3f381
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-02 19:40:17 +0530

    a js file of string object

---
+++ String Object/object.js
@@ -0,0 +1,13 @@
+var hello = "Hey Asritha";
+
+// hello = hello.toUpperCase();
+// hello = hello.charAt(4);
+
+ hello = hello.italics();
+ hello = hello.bold();
+
+// hello = hello.length; 
+
+document.write(hello);
+
+// string object in Javascript
