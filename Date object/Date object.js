90b069b68da0620949a1f4fa1ebfb2524877098f
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-02 20:13:42 +0530

    a js file of Date object

---
+++ Date object/Date object.js
@@ -0,0 +1,13 @@
+var todayDate = new Date();
+ 
+    // todayDate.getYear(2022);
+
+    // todayDate.setYear(2022);
+
+    // document.write(todayDate);
+
+var useString = todayDate.toDateString();
+
+document.write(useString);
+
+// Date object in Javascript
