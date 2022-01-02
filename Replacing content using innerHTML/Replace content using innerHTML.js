cd94a6821e364e982740b372395abdcb1b0f550d
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-02 21:34:10 +0530

    a js file of replace content using innerHTML

---
+++ Replacing content using innerHTML/Replace content using innerHTML.js
@@ -0,0 +1,13 @@
+function changeStyle() {
+    
+    var paragraph = document.getElementsByClassName ("para");
+
+    var changeText = paragraph[0].innerHTML = "New text is Green color";
+
+    var changeText = paragraph[1].innerHTML = "New text is purple color";
+
+    var changeText = paragraph[2].innerHTML = "New text is red color";
+
+    var changeText = paragraph[3].innerHTML = "New text is blue color";
+
+}
