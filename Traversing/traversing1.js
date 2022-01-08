08a752b8bb02d415c37281097e450c097b78d0a7
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-09 01:43:50 +0530

    a js file of traversing part 1

---
+++ Traversing/traversing1.js
@@ -0,0 +1,10 @@
+function changeText () {
+
+    var parent = document.getElementById('main');
+    var child = parent.lastElementChild;
+    child.style.color = "blue";
+
+
+    //  var child = parent.childNodes[1];
+
+}
