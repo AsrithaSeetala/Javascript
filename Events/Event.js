2eb78a455c803b9ee7ec6848f88aa1a765d1e61c
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-08 23:51:24 +0530

    a js file of event section

---
+++ Events/Event.js
@@ -0,0 +1,29 @@
+function changeColor() { 
+    
+    var text = document.getElementById("para1").style.color = "blue";
+
+}
+
+function changeBackground() { 
+    
+    var text = document.getElementById("para2").style.backgroundColor = "green";
+
+}
+
+function backToNormal() { 
+    
+    var text = document.getElementById("para2").style.backgroundColor = "";
+
+}
+
+function newPicture() { 
+    
+document.getElementById("image").src = "Madhuri.jpg";
+
+}
+
+function oldPicture() { 
+
+document.getElementById("image").src = "Madhuri.jpg";
+
+}
