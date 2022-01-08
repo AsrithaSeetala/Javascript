98b86087ddd42f8150c497043fa295f13ec32f0d
AsrithaSeetala <95120217+AsrithaSeetala@users.noreply.github.com>
2022-01-09 00:34:24 +0530

    a js file of Reading content using innerHTML

--- Reading content using innerHTML/Reading content using innerHTML.js
+++ Reading content using innerHTML/Reading content using innerHTML.js
@@ -1,17 +1,21 @@
 function changeStyle() {
     
-    var paragraph = document.getElementsByClassName ("para");
-
-    var element1 = paragraph[0].innerHTML;
-    var element2 = paragraph[1].innerHTML;
-    var element3 = paragraph[2].innerHTML;
-    var addThem = paragraph[3].innerHTML = element1 + element2 + element3;
-
-
-    var element1 = paragraph[0].innerHTML="";
-    var element2 = paragraph[1].innerHTML="";
-    var element3 = paragraph[2].innerHTML="";
+    var confirmation = confirm("confirming to submit");
     
-    document.getElementById("test").style.visiblity="hidden";
+    if (confirmation == true) {
+        
+        var paragraph = document.getElementsByClassName ("para");
 
+        var element1 = paragraph[0].innerHTML;
+        var element2 = paragraph[1].innerHTML;
+        var element3 = paragraph[2].innerHTML;
+        var addThem = paragraph[3].innerHTML = element1 + element2 + element3;
+
+
+        var element1 = paragraph[0].innerHTML="";
+        var element2 = paragraph[1].innerHTML="";
+        var element3 = paragraph[2].innerHTML="";
+
+        document.getElementById("test").style.visiblity="hidden";
+    }
 }
